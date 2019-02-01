11bdc23278675bcd4c6f69b120c38fa5b9dfd992
Yura <yurafil228@gmail.com>
2019-02-01 18:38:08 +0200

    header

---
+++ project/application/src/app/modules/shared/components/body/body.component.spec.ts
@@ -0,0 +1,6 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+
+import { BodyComponent } from './body.component';
+import { CoreModule } from '../../../core/core.module';
--- project/application/src/app/modules/shared/shared.module.ts
+++ project/application/src/app/modules/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LocalizationModule } from 'angular-l10n';
 
 import { HeaderComponent } from './components/header/header.component';
-
+import { BodyComponent } from './components/body/body.component';
 @NgModule({
     imports: [LocalizationModule, CommonModule],
-    exports: [LocalizationModule, CommonModule, HeaderComponent],
-    declarations: [HeaderComponent],
+    exports: [LocalizationModule, CommonModule, HeaderComponent, BodyComponent],
+    declarations: [HeaderComponent, BodyComponent],
 })
 export class SharedModule {}
+
