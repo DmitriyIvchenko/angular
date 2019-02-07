606536f28b2e959d5c4ae55c6b6eb70e50bfc8dd
yurii <yurafil228@gmail.com>
2019-02-07 21:09:31 +0300

    some change and footer

--- project/application/src/app/modules/shared/shared.module.ts
+++ project/application/src/app/modules/shared/shared.module.ts
@@ -4,10 +4,12 @@ import { LocalizationModule } from 'angular-l10n';
 
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
     imports: [LocalizationModule, CommonModule],
-    exports: [LocalizationModule, CommonModule, HeaderComponent, BodyComponent],
-    declarations: [HeaderComponent, BodyComponent],
+    exports: [LocalizationModule, CommonModule, HeaderComponent, BodyComponent, FooterComponent],
+    declarations: [HeaderComponent, BodyComponent, FooterComponent],
 })
 export class SharedModule {}
 
